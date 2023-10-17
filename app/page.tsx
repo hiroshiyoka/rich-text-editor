414d1dea994d6b667e7a7e0678c58d42161e1ac0
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-18 01:20:46 +0700

    feat: import form packages from react-hook-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
 export default function Home() {
   return <main className=""></main>;
 }
