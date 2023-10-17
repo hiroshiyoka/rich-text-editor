f9dba3fec5eefd921a78bab0f12fd143a844b258
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-18 01:23:16 +0700

    feat: apply useForm and add mode and defaultValues

--- app/page.tsx
+++ app/page.tsx
@@ -9,5 +9,13 @@ import {
 import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const form = useForm({
+    mode: "onChange",
+    defaultValues: {
+      title: "",
+      price: 29.99,
+      description: "",
+    },
+  });
   return <main className=""></main>;
 }
