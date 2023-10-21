a852d7696cb44bc447181803721652509547d965
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-21 12:44:03 +0700

    feat: create formSchema using z from zod

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,22 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 export default function Home() {
+  const formSchema = z.object({
+    title: z
+      .string()
+      .min(5, { message: "Hey the title is not long enough" })
+      .max(100, { message: "Its too long " }),
+    price: z.number().min(5, { message: "Hey the title is not long enough" }),
+    description: z
+      .string()
+      .min(5, { message: "Hey the title is not long enough" })
+      .max(100, { message: "Its too long" })
+      .trim(),
+  });
+
   const form = useForm({
     mode: "onChange",
     defaultValues: {
